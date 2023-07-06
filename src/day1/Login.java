8d1fc432580de13b72a90b14e6c7ae58590681e9
ziya <koltikin210@gmail.com>
2023-07-06 23:54:25 +0300

    login class was created in day1 package

---
+++ src/day1/Login.java
@@ -0,0 +1,9 @@
+package day1;
+
+public class Login {
+    public static void logIn(String user_name,String email ){
+        System.out.println("you are successfully login");
+        System.out.println("your user_name = " + user_name);
+        System.out.println("your email = " + email);
+    }
+}
