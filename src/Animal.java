9f962c286ff243a9449874a5886aef66c4c456b0
ziya <koltikin210@gmail.com>
2023-07-06 22:31:59 +0300

    initial commit

---
+++ src/Animal.java
@@ -0,0 +1,18 @@
+public class Animal {
+
+    private String name;
+    private int age;
+
+    Animal(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Animal{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
---
+++ src/Dog.java
@@ -0,0 +1,6 @@
+public class Dog extends Animal{
+
+    public Dog(String name, int age) {
+        super(name, age);
+    }
+}
