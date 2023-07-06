e1ea53189b3c8cc5d4b49e8e492883adacadfa30
ziya <koltikin210@gmail.com>
2023-07-06 22:33:45 +0300

    some changes on the Dog class

--- src/Dog.java
+++ src/Dog.java
@@ -1,4 +1,7 @@
 public class Dog extends Animal{
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
 
     public Dog(String name, int age) {
         super(name, age);
