50e4b90d136f30f369dd8069fe27fddbf81fedb0
ziya <koltikin210@gmail.com>
2023-07-07 08:14:55 +0300

    Car class updated

--- src/day2/Car.java
+++ src/day2/Car.java
@@ -28,6 +28,21 @@ public class Car {
         return year;
     }
 
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
 
 
 }
