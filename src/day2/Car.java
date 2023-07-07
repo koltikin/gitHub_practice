925c011a2b8c84cbbf13a681417bf7c5311c0ad8
ziya <koltikin210@gmail.com>
2023-07-07 08:13:11 +0300

    Car class created on the packed day2

---
+++ src/day2/Car.java
@@ -0,0 +1,33 @@
+package day2;
+
+public class Car {
+
+    private String brand, model,color;
+    private int year;
+
+    public Car(String brand, String model, String color, int year) {
+        this.brand = brand;
+        this.model = model;
+        this.color = color;
+        this.year = year;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+
+
+}
